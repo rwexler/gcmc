corrected for the volume calculation of NP, set r_min=0, update r_max after each iteration

io.py
@@ -165,8 +165,8 @@ class xsf_info(object) :
 	def get_r_min_max(self, buf_len) :
 		"""get minimum and maximum allowed r distance for NP growth"""
 		r_surf = np.max(np.linalg.norm(self.at_coord, axis=1))
-		self.r_min = r_surf - buf_len / 2
-		self.r_max = r_surf + buf_len / 2
+		self.r_min = 0.0
+		self.r_max = r_surf + buf_len
 
 	def get_vol(self) :
 		"""get volume of variable composition region"""
@@ -174,12 +174,18 @@ class xsf_info(object) :
 		self.vol = np.dot(np.cross(self.lat_vec[0], self.lat_vec[1]), (self.c_max - self.c_min) * c_unit)
 		return self.vol
 
+	def get_vol_np(self) :
+		"""get volume of variable composition region for nanoparticle"""
+		self.vol = 4.0 / 3 * np.pi * (self.r_max**3 - self.r_min**3)
+		return self.vol
+		
 	def pop_attr(self, filename, el, buf_len) :
 		"""populate attributes"""
 		self.init(filename, el)
 		self.get_c_min_max(buf_len)
 		self.get_r_min_max(buf_len)
 		self.get_vol()
+		self.get_vol_np()
 
 class qe_out_info(object) :
 	"""class for representing a QE output file

mc.py
@@ -309,11 +309,9 @@ class mc :
             trial = 0
             while dis < el.r_min[self.uvt_el_exc] or dis > el.r_max[self.uvt_el_exc] : # control atom distance
                 at_ad_coord  = np.random.rand(3) * 2 - 1
-                while np.linalg.norm(at_ad_coord) > 1 :
+                while np.linalg.norm(at_ad_coord) > 1 or np.linalg.norm(at_ad_coord) < xsf.r_min/xsf.r_max :
                     at_ad_coord  = np.random.rand(3) * 2 - 1
-                at_ad_coord /= np.linalg.norm(at_ad_coord)
-                rr = np.random.rand() * (xsf.r_max - xsf.r_min) + xsf.r_min
-                at_ad_coord *= rr
+                at_ad_coord *= xsf.r_max
                 dis = min([np.linalg.norm(at_ad_coord - xsf.at_coord[ind]) for ind in range(xsf.at_num)])
                 trial += 1
                 if trial >= 100000 :